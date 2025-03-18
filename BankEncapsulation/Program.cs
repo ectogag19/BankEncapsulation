b760bc2995609d9ff758ec7fcc6a3d04eb91a511
George Garcia <georgegarcia05012001@gmail.com>
2025-03-18 10:54:43 -0500

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" _____         _   _ _            _____         ");
+            Console.WriteLine("| __  |___ ___| |_|_|_|___ ___   |  _  |___ ___ ");
+            Console.WriteLine("| __ -| .'|   | '_| | |   | . |  |     | . | . |");
+            Console.WriteLine("|_____|__,|_|_|_,_|_|_|_|_|_  |  |__|__|  _|  _|");
+            Console.WriteLine("                          |___|        |_| |_|  ");
+            Console.WriteLine("                                                ");
+            Console.WriteLine("------------------------------------------------");
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            var georgesAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            
+            georgesAccount.Deposit(amountToDeposit);
+            Console.WriteLine($"Thank you! Your balance is now ${georgesAccount.GetBalance()}");
         }
     }
 }
